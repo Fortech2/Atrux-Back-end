28bf72b5f980b119e7fb77eeb98818498882a783
Darius <nicoaradarius2007@gmail.com>
2023-07-15 21:18:12 +0300

    Fix

--- website/models.py
+++ website/models.py
@@ -7,6 +7,7 @@ from uuid import uuid4
 class Driver(db.Model):
     __tablename__ = "driver"
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Integer)
     name = db.Column(db.String(150))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
