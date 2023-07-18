69833f2468eff49cb0e997e88674525ba93f9b80
Darius <nicoaradarius2007@gmail.com>
2023-07-18 12:48:33 +0300

    Add Flask-login

--- website/models.py
+++ website/models.py
@@ -3,19 +3,19 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 from uuid import uuid4
 
-
 class Driver(db.Model):
-    __tablename__ = "driver"
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Integer)
-    name = db.Column(db.String(150))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-class User(db.Model, UserMixin): 
-    __tablename__ = "user"
+    __tablename__ = 'drivers'
     id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(150))
     email = db.Column(db.String(345), unique=True) 
     password = db.Column(db.String(150))
-    drivers = db.relationship('Driver')
+    dispatcher_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+class User(db.Model, UserMixin): 
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(150))
+    email = db.Column(db.String(345), unique=True) 
+    password = db.Column(db.String(150))
+    phone_number = db.Column(db.Integer, unique=True)
+    drivers = db.relationship('Driver')
