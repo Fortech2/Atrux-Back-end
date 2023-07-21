769447b29c46edeb5186764297fbad72ab50401b
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-07-21 17:03:51 +0300

    get_data

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME = "database.db"
 def make_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:parola@localhost/test'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ioana123@localhost/test'
     db.init_app(app)
 
     from .views import views
