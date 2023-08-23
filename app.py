8b6c3c00e0c9e358403bcfecf2adf60ce78797ef
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-23 13:46:32 +0300

    new version for socket

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from website import make_app
-from flask_socketio import SocketIO
-import time
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
-app = make_app()
-
+app = Flask(__name__)
 socket_io = SocketIO(app, cors_allowed_origins="*")
+
 @socket_io.on('connect')
 def handle_connect():
     print('new connection')
@@ -12,8 +11,7 @@ def handle_connect():
 @socket_io.on('to-server')
 def handle_to_server(arg):
     print(f'new to-server event: {arg}')
-    socket_io.emit('from-server', str(time()))
+    emit('from-server', str(time()))
 
 if __name__ == '__main__':
-    socket_io = SocketIO(app, cors_allowed_origins="*")
     socket_io.run(app, port=50000)
