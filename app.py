0c9b179aca0ee976dc7d04c9489f1a2a08818fad
Darius <nicoaradarius2007@gmail.com>
2023-08-23 14:11:21 +0300

    Minor changes

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ socket_io = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 @socket_io.on('connect')
 def handle_connect():
     print('new connection')
+    socket_io.emit('from-server', 'hello from backend')
 
 @socket_io.on('disconnect')
 def handle_disconnect():
