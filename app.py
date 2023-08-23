708e26522818ef9c6651deb11815191bf9a64a4a
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-23 15:40:01 +0300

    new if

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from website import make_app
 from flask_socketio import SocketIO
-import time
-import gevent 
 
 app = make_app()
 
 socket_io = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+
 @socket_io.on('connect')
 def handle_connect():
     print('new connection')
@@ -19,7 +18,5 @@ def handle_to_server(arg):
     print(f'new to-server event: {arg}')
     socket_io.emit('from-server', 'hello from backend')  # Emit the message
 
-
 if __name__ == '__main__':
-    socket_io = SocketIO(app, cors_allowed_origins="*")
     socket_io.run(app, port=50000)
