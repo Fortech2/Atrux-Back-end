7b287ee833c5c3c13d43773b23cfadb51ece95a6
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-23 13:53:20 +0300

    another

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from website import make_app
+from flask_socketio import SocketIO
+import time
 
-app = Flask(__name__)
+app = make_app()
+
 socket_io = SocketIO(app, cors_allowed_origins="*")
-
 @socket_io.on('connect')
 def handle_connect():
     print('new connection')
@@ -11,7 +12,8 @@ def handle_connect():
 @socket_io.on('to-server')
 def handle_to_server(arg):
     print(f'new to-server event: {arg}')
-    emit('from-server', str(time()))
+    socket_io.emit('from-server', str(time()))
 
 if __name__ == '__main__':
+    socket_io = SocketIO(app, cors_allowed_origins="*")
     socket_io.run(app, port=50000)
