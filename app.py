a6c94e880fa041a3af54c6fae2436185154a6260
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-25 18:00:37 +0300

    notif changes

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 from website import make_app
 from flask_socketio import SocketIO
 #import geventwebsocket.gunicorn.workers
-
+from website.models import Driver
 app = make_app()
-
+connected_drivers = set() 
 socket_io = SocketIO(app, cors_allowed_origins="*") #, async_mode='gevent')
+def get_driver_email_by_id(driver_id):
+    driver = Driver.query.get(driver_id)
+    if driver:
+        return driver.email
+    return None
 
 @socket_io.on('connect')
-def handle_connect():
-    print('new connection')
-    socket_io.emit('from-server', 'hello from backend')
+def handle_connect(driver_id):
+    driver_email = get_driver_email_by_id(driver_id)
+    if driver_email:
+        connected_drivers.add(driver_id)
+        socket_io.join_room(driver_email)
+        print(f'Driver {driver_email} connected')
 
 @socket_io.on('disconnect')
-def handle_disconnect():
-    print('disconnected')
-
-@socket_io.on('to-server')
-def handle_to_server(arg):
-    print(f'new to-server event: {arg}')
-    socket_io.emit('from-server', 'hello from backend daisojfo iajoifjoiasfij')  # Emit the message
+def handle_disconnect(driver_id):
+    driver_email = get_driver_email_by_id(driver_id)
+    if driver_email in connected_drivers:
+        connected_drivers.remove(driver_id)
+        socket_io.leave_room(driver_email)
+        print(f'Driver {driver_email} disconnected')
 
 if __name__ == '__main__':
     gevent_worker = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
--- website/add_route.py
+++ website/add_route.py
@@ -1,11 +1,11 @@
 from flask_login import login_required, current_user
 from flask import Blueprint, request, make_response
 from .models import Driver
-from . import db
+from . import db, socket_io
 
 
 chat = Blueprint('chat', __name__)
-
+connected_drivers = set()
 @chat.route("/route", methods=['POST', "DELETE"])
 @login_required
 def add_route():
@@ -13,10 +13,20 @@ def add_route():
     driver_email = data["driver_email"]
     driver = Driver.query.filter_by(email=driver_email).first()
     
-    if request.method == "POST":
-        route = data["route"]
-        driver.route = route
+    if driver:
+        if request.method == "POST":
+            route = data["route"]
+            if driver.route != route:  # Check if the route actually changed
+                driver.route = route
+                db.session.commit()
+                if driver.email in connected_drivers:
+                    socket_io.emit('route-changed', {"message": "Route changed"}, room=driver.email)
+        else:
+            driver.route = ""
+            db.session.commit()
+            if driver.email in connected_drivers:
+                socket_io.emit('route-changed', {"message": "Route changed"}, room=driver.email)
+
+        return make_response("Route changed", 200)
     else:
-        driver.route = "" 
-    db.session.commit()
-    return make_response("Route changed", 200)
+        return make_response("Driver not found", 404)
