ef4f1b777529558e2df2ce3daa2592e43863b9b5
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-25 21:59:09 +0300

    corectura gevent

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from website import make_app
 from socketio_manager import socket_io
 
 app = make_app()
-socket_io.init_app(app) 
+socket_io.init_app(app)
 
 if __name__ == '__main__':
-    socket_io.run(app, host='0.0.0.0', port=5000, log_output=True)
+    socket_io.run(app, host='0.0.0.0', port=5000, log_output=True, server="gevent")
