f378b751ce8e56d9431e4868ea008ad9840dc94a
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-25 23:32:50 +0300

    email modif

--- socketio_manager.py
+++ socketio_manager.py
@@ -13,23 +13,17 @@ def get_driver_email_by_id(driver_id):
     return None
 
 @socket_io.on('connect')
-def handle_connect(driver_id):
-    driver_email = get_driver_email_by_id(driver_id)
-    if driver_email:
-        socket_io.join_room(driver_email)
-        print(f'Driver {driver_email} connected')
+def handle_connect(driver_email):
+    socket_io.join_room(driver_email)
+    print(f'Driver {driver_email} connected')
 
 @socket_io.on('disconnect')
-def handle_disconnect(driver_id):
-    driver_email = get_driver_email_by_id(driver_id)
-    if driver_email:
-        socket_io.leave_room(driver_email)
-        print(f'Driver {driver_email} disconnected')
+def handle_disconnect(driver_email):
+    socket_io.leave_room(driver_email)
+    print(f'Driver {driver_email} disconnected')
 
 @socket_io.on('route-changed')
 def handle_route_changed(data):
-    driver_id = data.get("driver_id") 
-    driver_email = get_driver_email_by_id(driver_id)
-    if driver_email:
-        socket_io.emit('custom-notification', {"message": data["route"]}, room=driver_email)
-        print(f'Route changed notification sent to Driver {driver_email}')
+    driver_email = data.get("driver_email") 
+    socket_io.emit('custom-notification', {"message": data["route"]}, room=driver_email)
+    print(f'Route changed notification sent to Driver {driver_email}')
