4884af25ce3d42edf6280f72841f40231222c0e6
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-26 13:45:06 +0300

    gevent new

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from website import make_app
 from socketio_manager import socket_io
+import geventwebsocket.gunicorn.workers
 
 app = make_app()
 socket_io.init_app(app)
-
 if __name__ == '__main__':
-    from gevent import monkey
-    monkey.patch_all()
-    socket_io.run(app, host='0.0.0.0', port=5000)
+    from gevent import pywsgi
+    
+    server = pywsgi.WSGIServer(("0.0.0.0", 50000), app, handler_class=geventwebsocket.gunicorn.workers.GeventWebSocketHandler)
+    server.serve_forever()
