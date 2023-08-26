e44b2e54c2980ca6c2e39c2211afdd3cf3918620
IoanaGavrila <109278542+IoanaGavrila@users.noreply.github.com>
2023-08-26 14:44:27 +0300

    trying

--- app.py
+++ app.py
@@ -5,7 +5,5 @@ import geventwebsocket.gunicorn.workers
 app = make_app()
 socket_io.init_app(app)
 if __name__ == '__main__':
-    from gevent import pywsgi
-    
-    server = pywsgi.WSGIServer(("0.0.0.0", 50000), app, handler_class=geventwebsocket.gunicorn.workers.GeventWebSocketHandler)
-    server.serve_forever()
+    gevent_worker = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
+    socket_io.run(app, port=50000, worker=gevent_worker)
--- socketio_manager.py
+++ socketio_manager.py
@@ -4,7 +4,7 @@ from flask_socketio import join_room, leave_room
 from website.auth import get_user_data
 
 
-socket_io = SocketIO(cors_allowed_origins="*", manage_session=True, async_mode="gevent")
+socket_io = SocketIO(cors_allowed_origins="*", manage_session=True)
 
 def get_driver_email_by_id(driver_id):
     driver = Driver.query.get(driver_id)
@@ -12,23 +12,28 @@ def get_driver_email_by_id(driver_id):
         return driver.email
     return None
 
+# @socket_io.on('connect')
+# def handle_connect():
+#     user_data_response = get_user_data()
+#     if user_data_response.status_code == 200:
+#         user_data = user_data_response.json()
+#         user_id = user_data["id"]
+#         driver_email = get_driver_email_by_id(user_id)
+#         if driver_email:
+#             join_room(driver_email)
+#             print(f'Driver {driver_email} connected')
+#         else:
+#             # Handle the case where user is not authenticated properly
+#             pass
+#     else:
+#         # Handle the case where user data retrieval failed
+#         pass
+
+
 @socket_io.on('connect')
 def handle_connect():
-    user_data_response = get_user_data()
-    if user_data_response.status_code == 200:
-        user_data = user_data_response.json()
-        user_id = user_data["id"]
-        driver_email = get_driver_email_by_id(user_id)
-        if driver_email:
-            join_room(driver_email)
-            print(f'Driver {driver_email} connected')
-        else:
-            # Handle the case where user is not authenticated properly
-            pass
-    else:
-        # Handle the case where user data retrieval failed
-        pass
-
+    print('new connection')
+    socket_io.emit('from-server', 'hello from backend')
 @socket_io.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
